53210acfcf1c1af1a57ab9faff9fd6e5ef2ad9b4
H. Meryem ÇİM <meryemcim80@gmail.com>
2021-10-19 19:07:34 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,22 @@
+console.log("Helllooo my world welcome")
+
+//JS type safe degildir.
+let dolarDun =9.20
+let dolarBugun=9.30
+
+console.log(dolarDun)
+
+const euroDun=11.2
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri=["Konut Kredisi", "Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("</ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+
+    console.log("</li>"+konutKredileri[i]+"</li>")
+
+}
+console.log("</ul>")
